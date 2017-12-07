a6bbe392fcb58613dd4674d03365b97f11205252
Alan Bevier <alanbevier@ziftsolutions.com>
2017-12-07 15:45:51 -0500

    Comments

--- localstack/ext/java/src/main/java/cloud/localstack/docker/LocalstackDockerTestRunner.java
+++ localstack/ext/java/src/main/java/cloud/localstack/docker/LocalstackDockerTestRunner.java
@@ -31,23 +31,25 @@ public class LocalstackDockerTestRunner extends BlockJUnit4ClassRunner {
 
     private static final Logger LOG = Logger.getLogger(LocalstackDockerTestRunner.class.getName());
 
-    public static final int API_GATEWAY_PORT = 4567;
-    public static final int KINESIS_PORT = 4568;
-    public static final int DYNAMO_PORT = 4569;
-    public static final int DYNAMO_STREAMS_PORT = 4570;
-    public static final int ELASTICSEARCH_PORT = 4571;
-    public static final int S3_PORT = 4572;
-    public static final int FIREHOSE_PORT = 4573;
-    public static final int LAMBDA_PORT = 4574;
-    public static final int SNS_PORT = 4575;
-    public static final int SQS_PORT = 4576;
-    public static final int REDSHIFT_PORT = 4577;
-    public static final int ES_PORT = 4578;
-    public static final int SES_PORT = 4579;
-    public static final int ROUTE53_PORT = 4580;
-    public static final int CLOUDFORMATION_PORT = 4581;
-    public static final int CLOUDWATCH_PORT = 4582;
-    public static final int SSM_PORT = 4583;
+    //These are the default internal ports in the localstack docker image, in order to 
+    //use them, they must be resolved to an external docker port via Container.getExternalPortFor()
+    public static final int API_GATEWAY_INTERNAL_PORT = 4567;
+    public static final int KINESIS_INTERNAL_PORT = 4568;
+    public static final int DYNAMO_INTERNAL_PORT = 4569;
+    public static final int DYNAMO_STREAMS_INTERNAL_PORT = 4570;
+    public static final int ELASTICSEARCH_INTERNAL_PORT = 4571;
+    public static final int S3_INTERNAL_PORT = 4572;
+    public static final int FIREHOSE_INTERNAL_PORT = 4573;
+    public static final int LAMBDA_INTERNAL_PORT = 4574;
+    public static final int SNS_INTERNAL_PORT = 4575;
+    public static final int SQS_INTERNAL_PORT = 4576;
+    public static final int REDSHIFT_INTERNAL_PORT = 4577;
+    public static final int ES_INTERNAL_PORT = 4578;
+    public static final int SES_INTERNAL_PORT = 4579;
+    public static final int ROUTE53_INTERNAL_PORT = 4580;
+    public static final int CLOUDFORMATION_INTERNAL_PORT = 4581;
+    public static final int CLOUDWATCH_INTERNAL_PORT = 4582;
+    public static final int SSM_INTERNAL_PORT = 4583;
 
     private static final Pattern READY_TOKEN = Pattern.compile("Ready\\.");
 
@@ -108,7 +110,7 @@ public class LocalstackDockerTestRunner extends BlockJUnit4ClassRunner {
 
 
     public static String getEndpointS3() {
-        String s3Endpoint = endpointForPort(S3_PORT);
+        String s3Endpoint = endpointForPort(S3_INTERNAL_PORT);
         /*
          * Use the domain name wildcard *.localhost.atlassian.io which maps to 127.0.0.1
          * We need to do this because S3 SDKs attempt to access a domain <bucket-name>.<service-host-name>
@@ -121,67 +123,67 @@ public class LocalstackDockerTestRunner extends BlockJUnit4ClassRunner {
 
 
     public static String getEndpointKinesis() {
-        return endpointForPort(KINESIS_PORT);
+        return endpointForPort(KINESIS_INTERNAL_PORT);
     }
 
     public static String getEndpointLambda() {
-        return endpointForPort(LAMBDA_PORT);
+        return endpointForPort(LAMBDA_INTERNAL_PORT);
     }
 
     public static String getEndpointDynamoDB() {
-        return endpointForPort(DYNAMO_PORT);
+        return endpointForPort(DYNAMO_INTERNAL_PORT);
     }
 
     public static String getEndpointDynamoDBStreams() {
-        return endpointForPort(DYNAMO_STREAMS_PORT);
+        return endpointForPort(DYNAMO_STREAMS_INTERNAL_PORT);
     }
 
     public static String getEndpointAPIGateway() {
-        return endpointForPort(API_GATEWAY_PORT);
+        return endpointForPort(API_GATEWAY_INTERNAL_PORT);
     }
 
     public static String getEndpointElasticsearch() {
-        return endpointForPort(ELASTICSEARCH_PORT);
+        return endpointForPort(ELASTICSEARCH_INTERNAL_PORT);
     }
 
     public static String getEndpointElasticsearchService() {
-        return endpointForPort(ES_PORT);
+        return endpointForPort(ES_INTERNAL_PORT);
     }
 
     public static String getEndpointFirehose() {
-        return endpointForPort(FIREHOSE_PORT);
+        return endpointForPort(FIREHOSE_INTERNAL_PORT);
     }
 
     public static String getEndpointSNS() {
-        return endpointForPort(SNS_PORT);
+        return endpointForPort(SNS_INTERNAL_PORT);
     }
 
     public static String getEndpointSQS() {
-        return endpointForPort(SQS_PORT);
+        return endpointForPort(SQS_INTERNAL_PORT);
     }
 
     public static String getEndpointRedshift() {
-        return endpointForPort(REDSHIFT_PORT);
+        return endpointForPort(REDSHIFT_INTERNAL_PORT);
     }
 
     public static String getEndpointSES() {
-        return endpointForPort(SES_PORT);
+        return endpointForPort(SES_INTERNAL_PORT);
     }
 
     public static String getEndpointRoute53() {
-        return endpointForPort(ROUTE53_PORT);
+        return endpointForPort(ROUTE53_INTERNAL_PORT);
     }
 
     public static String getEndpointCloudFormation() {
-        return endpointForPort(CLOUDFORMATION_PORT);
+        return endpointForPort(CLOUDFORMATION_INTERNAL_PORT);
     }
 
     public static String getEndpointCloudWatch() {
-        return endpointForPort(CLOUDWATCH_PORT);
+        return endpointForPort(CLOUDWATCH_INTERNAL_PORT);
     }
 
     public static String getEndpointSSM() {
-        return endpointForPort(SSM_PORT);
+        return endpointForPort(SSM_INTERNAL_PORT);
     }
 
 
